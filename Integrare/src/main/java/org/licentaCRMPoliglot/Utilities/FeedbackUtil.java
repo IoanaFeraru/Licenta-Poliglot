021851626d5845b09adc1a7973f1b4abf61ae03b
IoanaFeraru <147163108+IoanaFeraru@users.noreply.github.com>
2024-06-11 23:08:08 +0300

    Update FeedbackUtil.java

--- Integrare/src/main/java/org/licentaCRMPoliglot/Utilities/FeedbackUtil.java
+++ Integrare/src/main/java/org/licentaCRMPoliglot/Utilities/FeedbackUtil.java
@@ -2,16 +2,11 @@ package org.licentaCRMPoliglot.Utilities;
 
 import jakarta.nosql.mapping.document.DocumentTemplate;
 import org.licentaCRMPoliglot.Entities.ClientReferences.Feedback;
-import org.licentaCRMPoliglot.Entities.ClientReferences.FeedbackItem;
 import org.licentaCRMPoliglot.Entities.Produs.Produs;
 import org.licentaCRMPoliglot.Repositories.FeedbackRepo;
 import org.licentaCRMPoliglot.Repositories.ProdusRepository;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class FeedbackUtil {
-
     private final ProdusRepository produsRepository;
     private final DocumentTemplate template;
     private final FeedbackRepo feedbackRepo;
@@ -22,32 +17,17 @@ public class FeedbackUtil {
         this.feedbackRepo = feedbackRepo;
     }
 
-    public void updateProductRating(String codProdus) {
-        List<Feedback> feedbackList = feedbackRepo.findByCodProdus(codProdus);
-
-        if (feedbackList.isEmpty()) {
-            System.out.println("No feedback found for product: " + codProdus);
-            return;
-        }
-
-        List<Integer> ratings = feedbackList.stream()
-                .flatMap(feedback -> feedback.getFeedbackItems().stream())
-                .filter(item -> codProdus.equals(item.getCodProdus()))
-                .map(FeedbackItem::getRating)
-                .collect(Collectors.toList());
-
-        double averageRating = ratings.stream()
-                .mapToInt(Integer::intValue)
-                .average()
-                .orElse(0);
-
-        Produs produs = produsRepository.findById(codProdus);
+    public void updateProductRating(String productId, int rating) {
+        Produs produs = produsRepository.findById(productId);
 
         if (produs != null) {
-            produs.setRating(averageRating);
+            int newRatingCount = (produs.getRatingCount() == null) ? 1 : produs.getRatingCount() + 1;
+            double newRatingSum = (produs.getRatingSum() == null) ? rating : produs.getRatingSum() + rating;
+
+            produs.setRatingCount(newRatingCount);
+            produs.setRatingSum(newRatingSum);
+            produs.setRating(newRatingSum / newRatingCount);
             produsRepository.updateProdus(produs);
-        } else {
-            System.out.println("Product not found with code: " + codProdus);
         }
     }
 }
